ecbe3a3e16ec0f36386be5309a0fbc7fc0e4e372
joeTiger <joetiger05@gmail.com>
2016-07-21 08:09:50 +0300

    Add project files.

---
+++ TipTeam/Models/TipViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TipTeam.Models
+{
+    public class TipViewModel
+    {
+        public int Id { get; set; }
+        // The ProductName property is required.
+        [Required]
+        public string Data { get; set; }
+        [Required]
+        public string Un { get; set; }
+    }
+}
---
+++ TipTeam/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TipTeam.Startup))]
+namespace TipTeam
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
